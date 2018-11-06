9eaf755e00e882b4c126b81f83ef96810b0490db
N00B <eliasdecarvalhojunior@hotmail.com>
2018-11-06 16:54:21 -0200

    Update candidate list ion-avatar

--- src/components/components.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CandidateProfileDetailsComponent } from './candidate-profile-details/candidate-profile-details';
-@NgModule({
-	declarations: [CandidateProfileDetailsComponent],
-	imports: [],
-	exports: [CandidateProfileDetailsComponent]
-})
-export class ComponentsModule {}
