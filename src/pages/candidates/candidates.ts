43873700fe36986fb3be94fa66015c35222c9648
N00B <eliasdecarvalhojunior@hotmail.com>
2018-11-07 02:09:48 -0200

    Added candidate search

--- src/pages/candidates/candidates.ts
+++ src/pages/candidates/candidates.ts
@@ -11,8 +11,36 @@ import { NavController, IonicPage } from 'ionic-angular';
 })
 export class CandidatesPage {
 
+  searchQuery: string = '';
+  candidates: string[];
+
   constructor(public navCtrl: NavController) {
-    
+    this.initializeCandidates();
+  }
+
+  initializeCandidates() {
+    this.candidates = [
+      'N00B',
+      'Candidato 1',
+      'Candidato 2',
+      'Candidato 3',
+      'Candidato 4'
+    ];
+  }
+
+  getItems(ev: any) {
+    // Reset items back to all of the items
+    this.initializeCandidates();
+
+    // set val to the value of the searchbar
+    const val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.candidates = this.candidates.filter((candidate) => {
+        return (candidate.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
   }
 
   viewProfile(){
