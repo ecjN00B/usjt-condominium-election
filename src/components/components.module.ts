aa0a7e1d929e0c523dad2166f1dd6e69a953c654
Elias de Carvalho Junior <elias.de.carvalho@ibm.com>
2018-11-11 16:06:52 -0200

    Placed filenames in angular pattern

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { SocialCardComponent } from './social-card/social-card';
+import { SocialCardComponent } from './social-card/social-card.component';
 @NgModule({
 	declarations: [SocialCardComponent],
 	imports: [],
