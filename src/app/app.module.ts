1a7cb15a683ae6b2c6e9524071f41f71cb02c6c0
Elias de Carvalho Junior <elias.de.carvalho@ibm.com>
2018-11-12 19:32:44 -0200

    Integrated firebase to ionic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,23 @@ import { MyApp } from './app.component';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { AngularFireModule, FirebaseAppConfig } from 'angularfire2';
+
+const firebaseAppConfig: FirebaseAppConfig = {
+  apiKey: "AIzaSyCG5DzP_g7rjo_O15UDymcfv50YhwAJPkw",
+  authDomain: "ionic-condominium-election-app.firebaseapp.com",
+  databaseURL: "https://ionic-condominium-election-app.firebaseio.com",
+  projectId: "ionic-condominium-election-app",
+  storageBucket: "ionic-condominium-election-app.appspot.com",
+  messagingSenderId: "287146848223"
+};
+
 @NgModule({
   declarations: [
     MyApp
   ],
   imports: [
+    AngularFireModule.initializeApp(firebaseAppConfig),
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
