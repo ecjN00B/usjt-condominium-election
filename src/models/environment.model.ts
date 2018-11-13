fb91cb8dd1969ec9921339d87a2896dac58b4990
Elias de Carvalho Junior <elias.de.carvalho@ibm.com>
2018-11-13 18:36:12 -0200

    Rename environment model to the angular pattern

---
+++ src/models/environment.model.ts
@@ -0,0 +1,45 @@
+import * as data from '../env';
+
+export class Environment {
+    
+    private apiKey: string;
+    private authDomain: string;
+    private databaseURL: string;
+    private projectId: string;
+    private storageBucket: string;
+    private messagingSenderId: string;
+    
+    constructor(){
+        this.apiKey = data.process.env.apiKey;
+        this.authDomain = data.process.env.authDomain;
+        this.databaseURL = data.process.env.databaseURL;
+        this.projectId = data.process.env.projectId;
+        this.storageBucket = data.process.env.storageBucket;
+        this.messagingSenderId = data.process.env.messagingSenderId;
+    }
+
+    getApiKey():string {
+        return this.apiKey;
+    }
+
+    getAuthDomain():string {
+        return this.authDomain;
+    }
+    
+    getDatabaseUrl():string {
+        return this.databaseURL;
+    }
+
+    getProjectid():string {
+        return this.projectId;
+    }
+
+    getStorageBucket():string {
+        return this.storageBucket;
+    }
+
+    getMessagingSenderId():string {
+        return this.messagingSenderId;
+    }
+
+}
