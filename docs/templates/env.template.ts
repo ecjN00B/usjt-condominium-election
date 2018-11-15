1b16033c437c33294d79cc06e5a0d372ec0d55ca
Elias de Carvalho Junior <elias.de.carvalho@ibm.com>
2018-11-15 20:10:57 -0200

    Added .env template

---
+++ docs/templates/env.template.ts
@@ -0,0 +1,10 @@
+export const process = {
+    env: {
+        apiKey: "YOUR-FIREBASE-API-KEY",
+        authDomain: "YOUR-FIREBASE-AUTH-DOMAIN",
+        databaseURL: "YOUR-FIREBASE-DATABASE-URL",
+        projectId: "YOUR-FIREBASE-PROJECT-ID",
+        storageBucket: "YOUR-FIREBASE-STORAGE-BUCKET",
+        messagingSenderId: "YOUR-FIREBASE-MESSAGING-SENDER-ID"
+    }
+}
