757b6b818bfa0de6a5a7aa693b9e3dc99c61c2ba
Elias de Carvalho Junior <elias.de.carvalho@ibm.com>
2018-11-15 22:00:37 -0200

    Extended base service to handle errors in providers

--- src/providers/auth/auth.service.ts
+++ src/providers/auth/auth.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
 
 import { AngularFireAuth } from 'angularfire2/auth';
+import { BaseService } from '../base/base.service';
 
 @Injectable()
-export class AuthService {
+export class AuthService extends BaseService {
 
-  constructor(public afAuth: AngularFireAuth) {}
+  constructor(public afAuth: AngularFireAuth) {
+    super();
+  }
 
   createAuthUser(user: {email: string, password: string}): any {
-    return this.afAuth.auth.createUserWithEmailAndPassword(user.email, user.password);
+    return this.afAuth.auth.createUserWithEmailAndPassword(user.email, user.password)
+      .catch(this.handlePromiseError);
   }
 
 }
--- src/providers/user/user.service.ts
+++ src/providers/user/user.service.ts
@@ -3,19 +3,22 @@ import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Observable } from 'rxjs';
 import { User } from '../../models/user.model';
+import { BaseService } from '../base/base.service';
 
 @Injectable()
-export class UserService {
+export class UserService extends BaseService{
 
   users: Observable<User[]>;
 
   constructor(public db: AngularFireDatabase) {
+    super();
     this.users = db.list<User>(`/users`).valueChanges();
   }
 
   create(user: User, uuid:string): Promise<void> {
     return this.db.object(`/users/${uuid}`)
-      .set(user);
+      .set(user)
+      .catch(this.handlePromiseError);
   }
 
 }
